707f368920646cd8d6ec190561d9bcc889412d34
Ankit-Paunikar <ankitkpaunikar@gmail.com>
2024-08-15 20:33:04 +0530

    fixs

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const freshness = "7"; // Last 7 days
 
 const scrapeJobs = async () => {
   const browser = await puppeteer.launch({
-    executablePath: "/path/to/your/chrome",
+    executablePath: process.env.CHROME_EXECUTABLE_PATH,
     headless: false,
     args: ["--disable-notifications"],
   });
